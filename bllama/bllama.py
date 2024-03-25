b6d95b621d04d21f0990aae0e58f533f040bf5fa
Rafael Celente <rafael.celente@easymile.com>
2024-03-25 22:52:35 +0100

    feat: weight int8 quantization methods

--- bllama/bllama.py
+++ bllama/bllama.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn.functional as F
 from .config import bLlamaConfig
 from .llama import Transformer
+from .quantization import quantize_weights_to_int8
 from typing import Tuple
 
 class bLlama(pl.LightningModule):
@@ -14,6 +15,77 @@ class bLlama(pl.LightningModule):
         self.config = config
         self.model = Transformer(config)
 
+    def quantize_weights_to_int8(self):
+        current_state_dict = self.model.state_dict()
+        new_state_dict = {}
+        list_of_bitlinear_layers = ["attn.wq", "attn.wk", "attn.wv", "attn.wo", "ff.w1", "ff.w2", "ff.w3"]
+        for k, v in current_state_dict.items():
+            quantize_weights = False
+            for bitlinear_layer in list_of_bitlinear_layers:
+                if bitlinear_layer in k:
+                    quantize_weights = True
+                    break
+            new_state_dict[k] = quantize_weights_to_int8(v)[0] if quantize_weights else v
+        self.model.load_state_dict(new_state_dict)
+
+    def get_model_size_in_bytes(self, verbose=True):
+        param_size = 0
+        for param in self.model.parameters():
+            param_size += param.nelement() * param.element_size()
+
+        size_all_mb = (param_size) / 1024**2
+        if verbose: 
+            print(f'Model size:= {size_all_mb} MB')
+        return size_all_mb
+
+    def generate(
+            self,
+            prompt: str,
+            tokenizer: callable,
+            max_len: int = 50,
+            do_sample: bool = True, 
+            temperature: float = 0.1, 
+            top_k: int = 0,
+            repetition_penalty : float = 1.0, 
+            num_return_sequences : int = 1, 
+            device : str = "cuda",
+        ):
+        assert hasattr(tokenizer, 'encode'), f"Tokenizer {tokenizer.__name__} must have an encode method"
+        assert hasattr(tokenizer, 'decode'), f"Tokenizer {tokenizer.__name__} must have an decode method"
+        prompt_tokens = tokenizer.encode(prompt)
+        self.model = self.model.to(device)
+        self.model.eval()
+
+        def top_k_filtering(logits, top_k=0, filter_value=-float('Inf')):
+            top_k = min(top_k, logits.size(-1))
+            if top_k > 0:
+                indices_to_remove = logits < torch.topk(logits, top_k)[0][..., -1, None]
+                logits[indices_to_remove] = filter_value
+
+            return logits
+
+        for _ in range(num_return_sequences):
+            generated = torch.tensor([prompt_tokens])
+            generated = generated.to(device)
+
+            for _ in range(max_len):
+                with torch.no_grad():
+                    outputs = self.model(generated)
+                    next_token_logits = outputs[:, -1, :]
+                    for token in set(generated[0].tolist()):
+                        next_token_logits[:, token] /= repetition_penalty
+                    next_token_logits = next_token_logits / temperature
+                    filtered_logits = top_k_filtering(next_token_logits, top_k=top_k)
+                    if do_sample:
+                        next_token = torch.multinomial(F.softmax(filtered_logits, dim=-1), num_samples=1)
+                    else:
+                        next_token = torch.argmax(F.softmax(filtered_logits, dim=-1), dim=-1, keepdims=True)
+                    generated = torch.cat((generated, next_token), dim=-1)
+
+            result = generated[0].tolist()
+            text = tokenizer.decode(result)
+        return text
+    
     def training_step(
             self,
             batch: Tuple[torch.Tensor],
---
+++ bllama/quantization.py
@@ -0,0 +1,65 @@
+import torch
+from .utils import RMSNorm
+
+def weight_quant(w: torch.Tensor):
+    """
+    Quantize a set of weights to ternary based on the mean of the absolute values described by
+    https://github.com/microsoft/unilm/blob/master/bitnet/The-Era-of-1-bit-LLMs__Training_Tips_Code_FAQ.pdf
+
+    This doesn't change the type of the weights, but rather the values themselves.
+    Args:
+        w (torch.Tensor): weights
+    Returns:
+        u (torch.Tensor): quantized weights
+    """
+    scale = 1.0 / w.abs().mean().clamp_(min=1e-5)
+    u = (w * scale).round().clamp_(-1,1) / scale
+    return u
+
+def activation_quant(x: torch.Tensor):
+    """
+    Quantize the activation tensor to scaled 8 bit based on the mean of the absolute values described by
+    https://github.com/microsoft/unilm/blob/master/bitnet/The-Era-of-1-bit-LLMs__Training_Tips_Code_FAQ.pdf
+
+    This doesn't change the type of the activations, but rather the values themselves.
+    Args:
+        x (torch.Tensor): activations
+    Returns:
+        u (torch.Tensor): quantized weights
+    """
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    u = (x * scale).round().clamp_(-128,127) / scale
+    return u
+
+def activation_norm_quant(x: torch.Tensor):
+    """
+    Quantize the layer-normalized activations to 8 bit based on the mean of the absolute values described by
+    https://github.com/microsoft/unilm/blob/master/bitnet/The-Era-of-1-bit-LLMs__Training_Tips_Code_FAQ.pdf
+
+    This doesn't change the type of the activations, but rather the values themselves.
+    Args:
+        w (torch.Tensor): weights
+    Returns:
+        y (torch.Tensor): quantized activations
+        scale (torch.Tensor): scale factor
+    """
+    x = RMSNorm(x)
+    scale = 127.0 / x.abs().max(dim=-1, keepdim=True).values.clamp_(min=1e-5)
+    y = (x * scale).round().clamp_(-128,127)
+    return y, scale
+
+def quantize_weights_to_int8(w: torch.Tensor):
+    """
+    Offline quantization of a set of weights to int8 based on the mean of the absolute values described by
+    https://github.com/microsoft/unilm/blob/master/bitnet/The-Era-of-1-bit-LLMs__Training_Tips_Code_FAQ.pdf
+
+    This operation casts the weights to int8.
+    Args:
+        w (torch.Tensor): weights
+    Returns:
+        w_quant (torch.Tensor): quantized weights
+        scale (torch.Tensor): scale factor
+    """
+    scale = 1.0 / w.abs().mean().clamp_(min=1e-5)
+    w_quant = (w * scale).round().clamp_(-1,1) #.to(torch.int8)
+    return w_quant, scale
